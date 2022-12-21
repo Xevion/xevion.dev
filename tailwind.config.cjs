0e98916df002e2e67b32d2c876a795e331c75c81
Xevion <ryanchwalters@gmail.com>
2022-12-21 14:54:13 -0600

    Add fonts & hero-icons

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        manrope: ["\"Manrope\"", "sans-serif"],
+        opensans: ["\"Open Sans\"", "sans-serif"],
+        inter: ["\"Inter\"", "sans-serif"],
+        mono: ["\"Roboto Mono\"", "monospace"],
+        raleway: ["\"Raleway\"", "sans-serif"],
+        roboto: ["\"Roboto\""],
+      },
+    },
   },
   plugins: [],
 };
