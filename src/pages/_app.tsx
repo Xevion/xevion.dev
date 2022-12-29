fc80f00fb09e82a0027fcee8b0f6b12fb5d12816
Xevion <ryanchwalters@gmail.com>
2022-12-28 20:27:56 -0600

    Add @vercel/analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
-import { type AppType } from "next/app";
+import {type AppType} from "next/app";
 
-import { trpc } from "../utils/trpc";
+import {trpc} from "../utils/trpc";
 
 import "../styles/globals.scss";
+import {Analytics} from "@vercel/analytics/react";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const MyApp: AppType = ({Component, pageProps}) => {
+    return <>
+        <Component {...pageProps} />
+        <Analytics/>
+    </>;
 };
 
 export default trpc.withTRPC(MyApp);
