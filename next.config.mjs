4b2536aebead943658af8cd78f6673571414080b
Xevion <ryanchwalters@gmail.com>
2022-12-30 23:29:27 -0600

    Add redirects for v2 old site

--- next.config.mjs
+++ next.config.mjs
@@ -14,8 +14,15 @@ const config = {
         defaultLocale: "en",
     },
     async redirects() {
+        // Source cannot end with / slash
         return [
-            {source: '/resume', destination: '/resume.pdf', permanent: false}
+            {source: '/resume', destination: '/resume.pdf', permanent: false},
+            {source: '/2020/12/04/jekyll-github-pages-and-azabani', destination: 'https://v2.xevion.dev/2020/12/04/jekyll-github-pages-and-azabani/', permanent: false},
+            {source: '/2021/02/25/project-facelift-new-and-old', destination: 'https://v2.xevion.dev/2021/02/25/project-facelift-new-and-old/', permanent: false},
+            {source: '/2022/03/29/runnerspace-built-in-under-30-hours', destination: 'https://v2.xevion.dev/2022/03/29/runnerspace-built-in-under-30-hours/', permanent: false},
+            {source: '/2022/07/16/restricted-memory-and-data-framing-tricks', destination: 'https://v2.xevion.dev/2022/07/16/restricted-memory-and-data-framing-tricks/', permanent: false},
+            {source: '/photography', destination: 'https://v2.xevion.dev/photography/', permanent: false},
+            {source: '/drafts/presenting-to-humans', destination: 'https://v2.xevion.dev/drafts/presenting-to-humans/', permanent: false}
         ]
     }
 };
