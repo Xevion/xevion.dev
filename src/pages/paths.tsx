55fc7168b1f72851fe816ecd77fa7e86eb8a9f30
Xevion <ryanchwalters@gmail.com>
2022-12-30 18:29:18 -0600

    Add shell page for paths project

---
+++ src/pages/paths.tsx
@@ -0,0 +1,11 @@
+import {type NextPage} from "next";
+import AppWrapper from "../components/AppWrapper";
+import WorkInProgress from "../components/WorkInProgress";
+
+const PathsPage: NextPage = () => {
+    return <AppWrapper>
+        <WorkInProgress />
+    </AppWrapper>
+}
+
+export default PathsPage;
