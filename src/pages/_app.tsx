882f4291aa93d579c0f35586e62112f916d4d469
Xevion <ryanchwalters@gmail.com>
2023-02-27 14:04:43 -0600

    Add OGP banner to index

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,9 +5,21 @@ import { trpc } from "../utils/trpc";
 import "../styles/globals.scss";
 import { Analytics } from "@vercel/analytics/react";
 import { Provider } from "react-wrap-balancer";
+import Head from "next/head";
+import { OGP } from "react-ogp";
+
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <>
+      <Head>
+        <OGP
+          url="https://xevion.dev"
+          title="Xevion.dev"
+          description="The personal website of Xevion, a full-stack software developer."
+          siteName="xevion.dev"
+          image="https://xevion.dev/banner.png"
+        />
+      </Head>
       <Provider>
         <Component {...pageProps} />
       </Provider>
