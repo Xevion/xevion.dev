300f44fc34e4ca2108f2b34d50033850df53f33c
Xevion <xevion@xevion.dev>
2023-05-13 02:28:31 -0500

    Add link to v2.xevion.dev blog site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -107,6 +107,7 @@ export async function getStaticProps() {
 const buttons = [
   { text: "GitHub", href: "https://github.com/Xevion" },
   {text: "Projects", href: "/projects"},
+  {text: "Blog", href: "https://v2.xevion.dev/" },
   { text: "Contact", href: "/contact" },
   { text: "Resume", href: "/resume" },
 ];
