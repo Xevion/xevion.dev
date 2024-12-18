775203dc4bc3fbb80d759bd82e46932bf13e7004
Xevion <xevion@xevion.dev>
2024-12-18 14:00:08 -0600

    Provide data schema

--- src/utils/directus.ts
+++ src/utils/directus.ts
@@ -1,5 +1,13 @@
 import { createDirectus, rest } from "@directus/sdk";
 
-const directus = createDirectus("https://api.xevion.dev").with(rest());
+export type Schema = {
+  metadata: Metadata[];
+};
+
+export type Metadata = {
+  tagline: string;
+};
+
+const directus = createDirectus<Schema>("https://api.xevion.dev").with(rest());
 
 export default directus;
