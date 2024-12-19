dab475d78029791e1761aca0ecccba98f92e4772
Xevion <xevion@xevion.dev>
2024-12-19 16:34:08 -0600

    Update directus schema

--- src/utils/directus.ts
+++ src/utils/directus.ts
@@ -2,10 +2,36 @@ import { createDirectus, rest } from "@directus/sdk";
 
 export interface Schema {
   metadata: Metadata;
+  project: Project[];
+  technology: Technology[];
+}
+
+export interface Technology {
+  id: string;
+  name: string;
+  url: string | null;
+}
+
+export interface Project {
+  id: string;
+  name: string;
+  description: string;
+  shortDescription: string;
+  links: Link[];
+  wakatime_offset: number | null;
+  technologies: Technology[];
+}
+
+export interface Link {
+  icon: string;
+  url: string;
+  description: string | null;
 }
 
 export interface Metadata {
   tagline: string;
+  resume: string;
+  resumeFilename: string;
 }
 
 const directus = createDirectus<Schema>("https://api.xevion.dev").with(rest());
