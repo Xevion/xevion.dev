bd8e8999ecd14431706709ce79bcf5a4e8be05e9
Xevion <xevion@xevion.dev>
2024-12-26 16:14:32 -0600

    Remove react-ogp, use metadata object

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,21 +5,17 @@ import { trpc } from "@/utils/trpc";
 import "@/styles/globals.scss";
 import { Analytics } from "@vercel/analytics/react";
 import { Provider } from "react-wrap-balancer";
-import Head from "next/head";
-import { OGP } from "react-ogp";
+import { Metadata } from "next";
+  
+export const metadata: Metadata = {
+  title: "Xevion.dev",
+  description: "The personal website of Xevion, a full-stack software developer.",
+  applicationName: "xevion.dev",
+}
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <>
-      <Head>
-        <OGP
-          url="https://xevion.dev"
-          title="Xevion.dev"
-          description="The personal website of Xevion, a full-stack software developer."
-          siteName="xevion.dev"
-          image="https://xevion.dev/banner.png"
-        />
-      </Head>
       <Provider>
         <Component {...pageProps} />
       </Provider>
